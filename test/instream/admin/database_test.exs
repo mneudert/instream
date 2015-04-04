4105685000495ca91348d1c1b29c508a76bed9ba
Marc Neudert <marc.neudert@gmail.com>
2015-04-04 15:57:39 +0200

    improves database lifecycle test robustness

--- test/instream/admin/database_test.exs
+++ test/instream/admin/database_test.exs
@@ -4,18 +4,27 @@ defmodule Instream.Admin.DatabaseTest do
   alias Instream.Admin.Database
   alias Instream.TestHelpers.Connection
 
-  @database "test_database"
+  @database "test_database_lifecycle"
 
   test "database lifecycle" do
+    _ = @database |> Database.drop() |> Connection.execute()
+
+    # create test database
     creation = @database |> Database.create() |> Connection.execute()
     listing  = Database.show() |> Connection.execute()
 
     assert creation == %{results: [%{}]}
-    assert listing  == %{results: [%{rows: [%{columns: ["name"],values: [["test_database"]]}]}]}
+    assert %{results: [%{rows: [%{columns: ["name"], values: listing_values}]}]} = listing
 
+    assert Enum.any?(hd(listing_values), fn(db) -> db == @database end)
+
+    # delete test database
     deletion = @database |> Database.drop() |> Connection.execute()
+    listing  = Database.show() |> Connection.execute()
 
     assert deletion == %{results: [%{}]}
-    assert listing  == %{results: [%{rows: [%{columns: ["name"],values: [["test_database"]]}]}]}
+    assert %{results: [%{rows: [ listing_rows ]}]} = listing
+
+    refute Map.has_key?(listing_rows, :values)
   end
 end
