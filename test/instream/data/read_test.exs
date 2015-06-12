ca9dd89a5bc4f2128824d93b98f776b943050538
Marc Neudert <marc.neudert@gmail.com>
2015-06-12 18:50:51 +0200

    fixes "SELECT *" causing panics during tests

--- test/instream/data/read_test.exs
+++ test/instream/data/read_test.exs
@@ -1,5 +1,4 @@
 defmodule Instream.Data.ReadTest do
-  @moduledoc """
   use ExUnit.Case, async: true
 
   alias Instream.Data.Read
@@ -9,20 +8,19 @@ defmodule Instream.Data.ReadTest do
   @measurement "empty_measurement"
 
   test "read from empty measurement" do
-    query  = "SELECT * FROM #{ @measurement }" |> Read.query()
+    query  = "SELECT value FROM #{ @measurement }" |> Read.query()
     result = query |> Connection.execute(database: @database)
 
     assert %{ results: _ } = result
   end
 
   test "database in query string" do
-    query_in  = "SELECT * FROM \"#{ @database }\".\"default\".#{ @measurement }" |> Read.query
-    query_out = "SELECT * FROM #{ @measurement }" |> Read.query()
+    query_in  = "SELECT value FROM \"#{ @database }\".\"default\".\"#{ @measurement }\"" |> Read.query
+    query_out = "SELECT value FROM #{ @measurement }" |> Read.query()
 
     result_in  = query_in  |> Connection.execute()
     result_out = query_out |> Connection.execute(database: @database)
 
     assert result_in == result_out
   end
-  """
 end
