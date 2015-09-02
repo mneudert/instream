c02e4398bc91b343f7a557d524aaf87231694f05
Marc Neudert <marc.neudert@gmail.com>
2015-09-02 21:34:32 +0200

    tests for missing privilege errors

--- test/instream/auth_test.exs
+++ test/instream/auth_test.exs
@@ -3,6 +3,7 @@ defmodule Instream.AuthTest do
 
   alias Instream.Cluster.Database
   alias Instream.TestHelpers.AnonConnection
+  alias Instream.TestHelpers.GuestConnection
   alias Instream.TestHelpers.InvalidConnection
   alias Instream.TestHelpers.NotFoundConnection
   alias Instream.TestHelpers.QueryAuthConnection
@@ -20,10 +21,17 @@ defmodule Instream.AuthTest do
     refute Map.has_key?(result, :error)
   end
 
+
   test "invalid password" do
     %{ error: error } = Database.show() |> InvalidConnection.execute()
 
     assert String.contains?(error, "authentication failed")
+  end
+
+  test "privilege missing" do
+    %{ error: error } = "ignore" |> Database.drop() |> GuestConnection.execute()
+
+    assert String.contains?(error, "requires admin privilege")
   end
 
   test "user not found" do
