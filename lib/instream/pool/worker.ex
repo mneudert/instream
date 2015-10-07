874289c0ce1a10b67a615856f6580259b9627d6c
Marc Neudert <marc.neudert@gmail.com>
2015-10-07 19:17:14 +0200

    uses GenServer for poolboy workers

--- lib/instream/pool/worker.ex
+++ lib/instream/pool/worker.ex
@@ -3,33 +3,25 @@ defmodule Instream.Pool.Worker do
   Pool worker.
   """
 
+  use GenServer
+
   alias Instream.Query
 
   @behaviour :poolboy_worker
 
-  @doc """
-  Starts the worker process.
-  """
-  @spec start_link(Keyword.t) :: GenServer.on_start
   def start_link(conn) do
     GenServer.start_link(__MODULE__, conn)
   end
 
-  @doc """
-  Initializes the worker.
-  """
-  @spec init(Keyword.t) :: { :ok, Keyword.t }
   def init(conn), do: { :ok, conn }
 
 
   # GenServer callbacks
 
-  @doc false
   def handle_call({ :execute, query, opts }, _from, conn) do
     { :reply, execute(query, opts, conn), conn }
   end
 
-  @doc false
   def handle_cast({ :execute, query, opts }, conn) do
     execute(query, opts, conn)
 
