f8bc2b90b3661fc62394ba857e1ba7437369e61a
Marc Neudert <marc.neudert@gmail.com>
2015-11-27 18:50:13 +0100

    removes irrelevant test

--- test/instream/connection_test.exs
+++ test/instream/connection_test.exs
@@ -5,32 +5,6 @@ defmodule Instream.ConnectionTest do
   alias Instream.TestHelpers.UnreachableConnection
 
 
-  @otp_app    :instream_connection_test
-  @otp_config [ foo: :bar ]
-
-
-  defmodule TestConnection do
-    use Instream.Connection, otp_app: :instream_connection_test
-  end
-
-
-  setup_all do
-    Application.put_env(@otp_app, TestConnection, @otp_config)
-  end
-
-
-  test "configuration" do
-    config = [ writer:  Instream.Writer.Line,
-               otp_app: @otp_app ] ++ @otp_config
-
-    assert config == TestConnection.config
-  end
-
-  test "child_spec" do
-    assert { TestConnection, _, _, _, _, _ } = TestConnection.child_spec
-  end
-
-
   test "ping connection" do
     assert :pong  == Connection.ping()
     assert :error == UnreachableConnection.ping()
