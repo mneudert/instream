12f54314710994a015eba96194176ee0cf72a989
Marc Neudert <marc.neudert@gmail.com>
2015-11-29 13:44:57 +0100

    migrates data tests to connection namespace

--- test/instream/connection_test.exs
+++ test/instream/connection_test.exs
@@ -2,11 +2,147 @@ defmodule Instream.ConnectionTest do
   use ExUnit.Case, async: true
 
   alias Instream.TestHelpers.Connection
+  alias Instream.TestHelpers.GuestConnection
   alias Instream.TestHelpers.UnreachableConnection
+
+
+  @database "test_database"
+  @tags     %{ foo: "foo", bar: "bar" }
+
+
+  defmodule TestSeries do
+    use Instream.Series
+
+    series do
+      database    :test_database
+      measurement :data_write_struct
+
+      tag :bar
+      tag :foo
+
+      field :value
+    end
+  end
 
 
   test "ping connection" do
     assert :pong  == Connection.ping()
     assert :error == UnreachableConnection.ping()
+  end
+
+
+  test "read from empty measurement" do
+    query  = "SELECT value FROM empty_measurement"
+    result = query |> Connection.query(database: @database)
+
+    assert %{ results: _ } = result
+  end
+
+  test "read using database in query string" do
+    query_in  = "SELECT value FROM \"#{ @database }\".\"default\".\"empty_measurement\""
+    query_out = "SELECT value FROM empty_measurement"
+
+    result_in  = query_in  |> Connection.query()
+    result_out = query_out |> Connection.query(database: @database)
+
+    assert result_in == result_out
+  end
+
+
+  test "write data" do
+    measurement = "write_data"
+    data        = %{
+      database: @database,
+      points: [
+        %{
+          measurement: measurement,
+          tags: @tags,
+          fields: %{ value: 0.66 }
+        }
+      ]
+    }
+
+    assert :ok == data |> Connection.write()
+
+    # wait to ensure data was written
+    :timer.sleep(250)
+
+    # check data
+    query  = "SELECT * FROM #{ measurement } GROUP BY *"
+    result = query |> Connection.query(database: @database)
+
+    %{ results: [%{ series: [%{ tags: values_tags,
+                                values: value_rows }]}]} = result
+
+    assert @tags == values_tags
+    assert 0 < length(value_rows)
+  end
+
+  test "write data async" do
+    measurement = "write_data_async"
+    data        = %{
+      database: @database,
+      points: [
+        %{
+          measurement: measurement,
+          tags: @tags,
+          fields: %{ value: 0.99 }
+        }
+      ]
+    }
+
+    assert :ok == data |> Connection.write(async: true)
+
+    # wait to ensure data was written
+    :timer.sleep(250)
+
+    # check data
+    query  = "SELECT * FROM #{ measurement } GROUP BY *"
+    result = query |> Connection.query(database: @database)
+
+    %{ results: [%{ series: [%{ tags: values_tags,
+                                values: value_rows }]}]} = result
+
+    assert @tags == values_tags
+    assert 0 < length(value_rows)
+  end
+
+
+  test "writing series struct" do
+    data = %TestSeries{}
+    data = %{ data | fields: %{ data.fields | value: 17 }}
+    data = %{ data | tags:   %{ data.tags   | foo: "foo", bar: "bar" }}
+
+    assert :ok == data |> Connection.write()
+
+    # wait to ensure data was written
+    :timer.sleep(250)
+
+    # check data
+    query  = "SELECT * FROM data_write_struct GROUP BY *"
+    result = query |> Connection.query(database: @database)
+
+    %{ results: [%{ series: [%{ tags: values_tags,
+                                values: value_rows }]}]} = result
+
+    assert @tags == values_tags
+    assert 0 < length(value_rows)
+  end
+
+
+  test "write data with missing privileges" do
+    data = %{
+      database: @database,
+      points: [
+        %{
+          measurement: "write_data_privileges",
+          fields: %{ value: 0.66 }
+        }
+      ]
+    }
+
+    %{ error: error } = data |> GuestConnection.write()
+
+    assert String.contains?(error, "not authorized")
   end
 end
