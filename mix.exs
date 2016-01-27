4f40fc0e0f605cb8073f14d385ea1b35fb4650b5
Marc Neudert <marc.neudert@gmail.com>
2016-01-27 19:30:27 +0100

    modernizes mix file

--- mix.exs
+++ mix.exs
@@ -4,50 +4,48 @@ defmodule Instream.Mixfile do
   @url_github "https://github.com/mneudert/instream"
 
   def project do
-    [ app:           :instream,
-      name:          "Instream",
+    [ app:     :instream,
+      name:    "Instream",
+      version: "0.10.0-dev",
+      elixir:  "~> 1.0",
+      deps:    deps,
+
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+
       description:   "InfluxDB driver for Elixir",
-      package:       package,
-      version:       "0.10.0-dev",
-      elixir:        "~> 1.0",
-      deps:          deps(Mix.env),
       docs:          docs,
-      test_coverage: [ tool: ExCoveralls ]]
+      package:       package,
+      test_coverage: [ tool: ExCoveralls ] ]
   end
 
   def application do
-    [ applications: [ :hackney, :poison, :poolboy ]]
+    [ applications: [ :hackney, :poison, :poolboy ] ]
   end
 
-  def deps(:docs) do
-    deps(:prod) ++
-      [ { :earmark, "~> 0.2",  optional: true },
-        { :ex_doc,  "~> 0.11", optional: true } ]
-  end
+  defp deps do
+    [ { :earmark, "~> 0.2",  only: :docs },
+      { :ex_doc,  "~> 0.11", only: :docs },
 
-  def deps(:test) do
-    deps(:prod) ++
-      [ { :dialyze,     "~> 0.2", optional: true },
-        { :excoveralls, "~> 0.4", optional: true } ]
-  end
+      { :dialyze,     "~> 0.2", only: :test },
+      { :excoveralls, "~> 0.4", only: :test },
 
-  def deps(_) do
-    [ { :hackney, "~> 1.1" },
+      { :hackney, "~> 1.1" },
       { :poison,  "~> 1.4 or ~> 2.0" },
       { :poolboy, "~> 1.5" } ]
   end
 
-  def docs do
+  defp docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "readme",
       source_ref: "master",
       source_url: @url_github ]
   end
 
-  def package do
+  defp package do
     %{ files:       [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
        licenses:    [ "Apache 2.0" ],
        links:       %{ "GitHub" => @url_github },
-       maintainers: [ "Marc Neudert" ]}
+       maintainers: [ "Marc Neudert" ] }
   end
 end
