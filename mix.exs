394841a5e6ca287c06d598522520971a1a37e1a8
Marc Neudert <marc.neudert@gmail.com>
2016-02-06 17:11:23 +0100

    configures mix cli environments

--- mix.exs
+++ mix.exs
@@ -13,6 +13,15 @@ defmodule Instream.Mixfile do
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
 
+      preferred_cli_env: [
+        coveralls:          :test,
+        'coveralls.detail': :test,
+        'coveralls.travis': :test,
+        dialyze:            :test,
+        docs:               :docs,
+        'hex.docs':         :docs
+      ],
+
       description:   "InfluxDB driver for Elixir",
       docs:          docs,
       package:       package,
