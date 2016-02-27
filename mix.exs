d410a691105a67a68c5f8eef17b68bbf50d7af42
Marc Neudert <marc.neudert@gmail.com>
2016-02-27 16:12:26 +0100

    bumps version to v0.11.0-dev

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Instream.Mixfile do
   def project do
     [ app:     :instream,
       name:    "Instream",
-      version: "0.10.0",
+      version: "0.11.0-dev",
       elixir:  "~> 1.0",
       deps:    deps,
 
@@ -47,7 +47,7 @@ defmodule Instream.Mixfile do
   defp docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "readme",
-      source_ref: "v0.10.0",
+      source_ref: "master",
       source_url: @url_github ]
   end
 
