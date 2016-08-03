a153bbdcda812f721098f939ab685cc2c91789ef
Marc Neudert <marc.neudert@gmail.com>
2016-08-03 19:53:23 +0200

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule Instream.Mixfile do
   end
 
   defp deps do
-    [ { :earmark, "~> 0.2",  only: :docs },
-      { :ex_doc,  "~> 0.11", only: :docs },
+    [ { :ex_doc, "~> 0.13", only: :docs }, # requires elixir ~> 1.2
 
       { :dialyze,     "~> 0.2", only: :test },
       { :excoveralls, "~> 0.5", only: :test },
