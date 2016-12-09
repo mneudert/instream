b86ae387a68312e4bd0153982b1305d531f508f7
Marc Neudert <marc.neudert@gmail.com>
2016-12-09 19:02:59 +0100

    improves writer callback doc

--- lib/instream/writer.ex
+++ lib/instream/writer.ex
@@ -15,8 +15,12 @@ defmodule Instream.Writer do
 
   @doc """
   Writes a point.
+
+  The `worker_state` parameter contains the state of the worker requesting the
+  write operation. Please refer to the `Instream.Pool.Worker` module to see
+  which parameters are available.
   """
-  @callback write(payload :: Query.t,
-                  opts    :: Keyword.t,
-                  conn    :: map) :: Response.t
+  @callback write(payload              :: Query.t,
+                  opts                 :: Keyword.t,
+                  worker_state :: map) :: Response.t
 end
