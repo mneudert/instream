ff8a7de565a8827ff99e682394611405b82494c1
Marc Neudert <marc.neudert@gmail.com>
2016-12-18 12:32:25 +0100

    drops :dialyze dependency

--- mix.exs
+++ mix.exs
@@ -16,8 +16,7 @@ defmodule Instream.Mixfile do
       preferred_cli_env: [
         coveralls:          :test,
         'coveralls.detail': :test,
-        'coveralls.travis': :test,
-        dialyze:            :test
+        'coveralls.travis': :test
       ],
 
       description:   "InfluxDB driver for Elixir",
@@ -33,7 +32,6 @@ defmodule Instream.Mixfile do
   defp deps do
     [ { :ex_doc, ">= 0.0.0", only: :dev },
 
-      { :dialyze,     "~> 0.2", only: :test },
       { :excoveralls, "~> 0.5", only: :test },
 
       { :hackney, "~> 1.1" },
