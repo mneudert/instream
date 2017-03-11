a4d28b8a1405b2dfef08e0383e0a1ea4d22194e7
Marc Neudert <marc.neudert@gmail.com>
2017-03-11 14:00:34 +0100

    ws

--- mix.exs
+++ mix.exs
@@ -32,9 +32,8 @@ defmodule Instream.Mixfile do
   end
 
   defp deps do
-    [ { :ex_doc, ">= 0.0.0", only: :dev },
-
-      { :excoveralls, "~> 0.6", only: :test },
+    [ { :ex_doc,      ">= 0.0.0", only: :dev },
+      { :excoveralls, "~> 0.6",   only: :test },
 
       { :hackney, "~> 1.1" },
       { :poison,  "~> 2.0 or ~> 3.0" },
