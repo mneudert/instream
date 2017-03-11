70dbfa62872a7b5555a48b76faf02114da2fdbb0
Marc Neudert <marc.neudert@gmail.com>
2017-03-11 14:00:13 +0100

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Instream.Mixfile do
   defp deps do
     [ { :ex_doc, ">= 0.0.0", only: :dev },
 
-      { :excoveralls, "~> 0.5", only: :test },
+      { :excoveralls, "~> 0.6", only: :test },
 
       { :hackney, "~> 1.1" },
       { :poison,  "~> 2.0 or ~> 3.0" },
