0ea3ac5634e43156d4f1c6bc9e0766ab4e01ee37
Marc Neudert <marc.neudert@gmail.com>
2018-09-22 12:23:32 +0200

    simplifies graph pool spec fetching

--- lib/instream/pool/spec.ex
+++ lib/instream/pool/spec.ex
@@ -8,18 +8,12 @@ defmodule Instream.Pool.Spec do
   """
   @spec spec(module) :: Supervisor.Spec.spec()
   def spec(conn) do
-    {pool_opts, worker_opts} = get_opts(conn)
-
-    :poolboy.child_spec(conn, pool_opts, worker_opts)
-  end
-
-  defp get_opts(conn) do
     pool_opts =
       (conn.config([:pool]) || [])
       |> Keyword.take([:size, :max_overflow])
       |> Keyword.put(:name, {:local, conn.__pool__})
       |> Keyword.put(:worker_module, Pool.Worker)
 
-    {pool_opts, %{module: conn}}
+    :poolboy.child_spec(conn, pool_opts, %{module: conn})
   end
 end
