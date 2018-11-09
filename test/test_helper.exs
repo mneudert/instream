83e54bf92270ee77c9999259f324c219d74d5243
Marc Neudert <marc.neudert@gmail.com>
2018-11-09 21:13:37 +0100

    adds influxdb 1.7 test tag

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -50,7 +50,7 @@ config =
       config
 
     {:ok, version} ->
-      versions = ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6"]
+      versions = ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7"]
       config = Keyword.put(config, :exclude, config[:exclude] || [])
 
       Enum.reduce(versions, config, fn ver, acc ->
