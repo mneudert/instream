ed0b4802cbf30ff410172404a01096586796fadc
Marc Neudert <marc.neudert@gmail.com>
2018-11-27 21:11:10 +0100

    [fixes #46] fixes Query struct typespec

--- lib/instream/query.ex
+++ lib/instream/query.ex
@@ -8,9 +8,9 @@ defmodule Instream.Query do
             opts: [],
             type: nil
 
-  @type payload_type :: String.t() | map
+  @type payload_type :: String.t() | map | nil
   @type query_method :: :get | :post
-  @type query_type :: :ping | :read | :write
+  @type query_type :: :ping | :read | :status | :version | :write
 
   @type t :: %__MODULE__{
           method: query_method,
