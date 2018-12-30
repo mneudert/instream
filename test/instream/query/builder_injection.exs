4e79c16d74ac803f923dccbf40df4db9a4666ed8
Marc Neudert <marc.neudert@gmail.com>
2018-12-30 12:39:09 +0100

    ensures influxql injection prevention is working

---
+++ test/instream/query/builder_injection.exs
@@ -0,0 +1,18 @@
+defmodule Instream.Query.BuilderInjectionTest do
+  use ExUnit.Case, async: true
+
+  alias Instream.Encoder.InfluxQL
+  alias Instream.Query.Builder
+  alias Instream.TestHelpers.Connections.DefaultConnection
+
+  test "SELECT InfluxQL Injection" do
+    # result if escaping is broken:
+    # %{error: "error parsing query: found  FROM injectable, expected FROM at line 1, char 29"}
+    assert %{results: [_]} =
+      "injectable"
+      |> Builder.from()
+      |> Builder.select(["some", ~S(broken ';stuff")])
+      |> InfluxQL.encode()
+      |> DefaultConnection.query(database: "test_database")
+  end
+end
