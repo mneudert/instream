55114d80c497135f369f6354651814198cb9eaad
Marc Neudert <marc.neudert@gmail.com>
2019-05-07 18:40:22 +0200

    Improve line encoder performance

--- lib/instream/encoder/line.ex
+++ lib/instream/encoder/line.ex
@@ -22,46 +22,47 @@ defmodule Instream.Encoder.Line do
 
   defp encode([point | points], lines) do
     line =
-      point.measurement
-      |> encode_property()
+      [encode_property(point.measurement)]
       |> append_tags(point)
       |> append_fields(point)
       |> append_timestamp(point)
+      |> Enum.join()
 
     encode(points, [line | lines])
   end
 
   defp append_fields(line, %{fields: fields}) do
-    "#{line} #{encode_fields(fields)}"
+    fields
+    |> Enum.reduce([], fn
+      {_, nil}, acc -> acc
+      {field, value}, acc -> [",", encode_property(field), "=", encode_value(value) | acc]
+    end)
+    |> case do
+      [] -> line
+      ["," | encoded_fields] -> [line, " " | encoded_fields]
+    end
   end
 
   defp append_tags(line, %{tags: tags}) do
     tags
     |> Enum.reduce([], fn
       {_, nil}, acc -> acc
-      {tag, value}, acc -> ["#{encode_property(tag)}=#{encode_property(value)}" | acc]
+      {tag, value}, acc -> [",", encode_property(tag), "=", encode_property(value) | acc]
     end)
-    |> List.insert_at(0, line)
-    |> Enum.join(",")
+    |> case do
+      [] -> line
+      encoded_tags -> [line | encoded_tags]
+    end
   end
 
   defp append_tags(line, _), do: line
 
   defp append_timestamp(line, %{timestamp: nil}), do: line
-  defp append_timestamp(line, %{timestamp: ts}), do: "#{line} #{ts}"
+  defp append_timestamp(line, %{timestamp: ts}), do: [line, " ", ts]
   defp append_timestamp(line, _), do: line
 
-  defp encode_fields(fields) do
-    fields
-    |> Enum.reduce([], fn
-      {_, nil}, acc -> acc
-      {field, value}, acc -> ["#{encode_property(field)}=#{encode_value(value)}" | acc]
-    end)
-    |> Enum.join(",")
-  end
-
-  defp encode_value(i) when is_integer(i), do: "#{i}i"
-  defp encode_value(s) when is_binary(s), do: "\"#{String.replace(s, "\"", "\\\"")}\""
+  defp encode_value(i) when is_integer(i), do: [Integer.to_string(i), "i"]
+  defp encode_value(s) when is_binary(s), do: ["\"", String.replace(s, "\"", "\\\""), "\""]
   defp encode_value(true), do: "true"
   defp encode_value(false), do: "false"
   defp encode_value(other), do: inspect(other)
