42aa07499574182c7d8ef7b088bb7e2edc314b94
Marc Neudert <marc.neudert@gmail.com>
2019-06-29 14:53:18 +0200

    Merge logging tests

---
+++ test/instream/log/default_logger_test.exs
@@ -0,0 +1,142 @@
+defmodule Instream.Log.DefaultLoggerTest do
+  use ExUnit.Case, async: false
+
+  import ExUnit.CaptureLog
+
+  defmodule LogConnection do
+    use Instream.Connection,
+      config: [
+        auth: [method: :query, username: "instream_test", password: "instream_test"]
+      ]
+  end
+
+  defmodule TestSeries do
+    use Instream.Series
+
+    series do
+      database "test_database"
+      measurement "log_write_entry_test"
+
+      tag :t
+
+      field :f
+    end
+  end
+
+  setup do
+    {:ok, _} = start_supervised(LogConnection)
+    :ok
+  end
+
+  test "logging ping request" do
+    log =
+      capture_log(fn ->
+        :pong = LogConnection.ping()
+
+        :timer.sleep(10)
+      end)
+
+    assert String.contains?(log, "ping")
+    assert String.contains?(log, "pong")
+
+    assert String.contains?(log, LogConnection.config([:host]))
+
+    assert String.contains?(log, "query_time=")
+    assert String.contains?(log, "response_status=204")
+  end
+
+  test "logging read request" do
+    query = "SELECT value FROM empty_measurement"
+
+    log =
+      capture_log(fn ->
+        _ = LogConnection.query(query)
+
+        :timer.sleep(10)
+      end)
+
+    assert String.contains?(log, "query")
+    assert String.contains?(log, query)
+
+    assert String.contains?(log, "query_time=")
+    assert String.contains?(log, "response_status=200")
+  end
+
+  test "logging read request with redacted password" do
+    query = ~s(CREATE USER "instream_test" WITH PASSWORD "instream_test")
+
+    log =
+      capture_log(fn ->
+        _ = LogConnection.query(query)
+
+        :timer.sleep(10)
+      end)
+
+    assert String.contains?(log, "CREATE USER")
+    refute String.contains?(log, ~s(PASSWORD "instream_test"))
+  end
+
+  test "logging status request" do
+    log =
+      capture_log(fn ->
+        :ok = LogConnection.status()
+
+        :timer.sleep(10)
+      end)
+
+    assert String.contains?(log, "status")
+    assert String.contains?(log, "ok")
+
+    assert String.contains?(log, LogConnection.config([:host]))
+
+    assert String.contains?(log, "query_time=")
+    assert String.contains?(log, "response_status=204")
+  end
+
+  test "logging write request" do
+    points = [
+      %TestSeries{
+        tags: %TestSeries.Tags{t: "foo"},
+        fields: %TestSeries.Fields{f: "foo"}
+      },
+      %TestSeries{
+        tags: %TestSeries.Tags{t: "bar"},
+        fields: %TestSeries.Fields{f: "bar"}
+      }
+    ]
+
+    log =
+      capture_log(fn ->
+        :ok = LogConnection.write(points)
+
+        :timer.sleep(10)
+      end)
+
+    assert String.contains?(log, "write")
+    assert String.contains?(log, "#{length(points)} points")
+
+    assert String.contains?(log, "query_time=")
+    assert String.contains?(log, "response_status=0")
+  end
+
+  describe "passing [log: false]" do
+    test "not logging ping requests" do
+      assert "" ==
+               capture_log(fn ->
+                 :pong = LogConnection.ping(log: false)
+
+                 :timer.sleep(10)
+               end)
+    end
+
+    test "not logging read request" do
+      assert "" ==
+               capture_log(fn ->
+                 query = "SELECT value FROM empty_measurement"
+                 _ = LogConnection.query(query, log: false)
+
+                 :timer.sleep(10)
+               end)
+    end
+  end
+end
