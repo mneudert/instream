1a3000c497d77ab9680bcf8ed38ef0ea97d6363f
Marc Neudert <marc.neudert@gmail.com>
2019-06-30 16:04:53 +0200

    Inline inets connection timeout test handler

--- test/instream/connection/timeout_test.exs
+++ test/instream/connection/timeout_test.exs
@@ -9,14 +9,50 @@ defmodule Instream.Connection.TimeoutTest do
       ]
   end
 
+  defmodule InetsHandler do
+    require Record
+
+    Record.defrecord(:mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl"))
+
+    def unquote(:do)(mod_data), do: serve_uri(mod(mod_data, :request_uri), mod_data)
+
+    defp serve_uri('/query?db=timeout', _mod_data) do
+      :timer.sleep(100)
+
+      body = '{"results": [{}]}'
+
+      head = [
+        code: 200,
+        content_length: body |> length() |> String.to_charlist(),
+        content_type: 'application/json'
+      ]
+
+      {:proceed, [{:response, {:response, head, body}}]}
+    end
+
+    defp serve_uri('/query?db=timeout_long', _mod_data) do
+      :timer.sleep(10_000)
+
+      body = '{"results": [{}]}'
+
+      head = [
+        code: 200,
+        content_length: body |> length() |> String.to_charlist(),
+        content_type: 'application/json'
+      ]
+
+      {:proceed, [{:response, {:response, head, body}}]}
+    end
+  end
+
   setup_all do
     root = String.to_charlist(__DIR__)
 
     httpd_config = [
       document_root: root,
-      modules: [Instream.TestHelpers.Inets.Handler],
+      modules: [InetsHandler],
       port: 0,
-      server_name: 'instream_testhelpers_inets_handler',
+      server_name: 'instream_connection_timeout_test',
       server_root: root
     ]
 
