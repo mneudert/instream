0609406d7e350f9a8fb2a1d48d448df763dfb90d
Marc Neudert <marc.neudert@gmail.com>
2020-04-14 18:06:00 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Instream.MixProject do
     [
       {:benchee, "~> 1.0", only: :bench, runtime: false},
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test, runtime: false},
       {:hackney, "~> 1.1"},
