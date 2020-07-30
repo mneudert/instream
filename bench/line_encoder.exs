c3b65bbd79d4519f51be5fe1c2d81bf4c687d7d1
Marc Neudert <marc.neudert@gmail.com>
2020-07-30 20:10:00 +0200

    Remove module wrapping from benchmark

--- bench/line_encoder.exs
+++ bench/line_encoder.exs
@@ -1,52 +1,46 @@
-defmodule Instream.Benchmark.LineEncoder do
-  alias Instream.Encoder.Line
+alias Instream.Encoder.Line
 
-  @point_complete %{
-    measurement: "disk_free",
-    fields: %{
-      value: 442_221_834_240
-    },
-    tags: %{
-      hostname: "server01"
-    },
-    timestamp: 1_435_362_189_575_692_182
-  }
+point_complete = %{
+  measurement: "disk_free",
+  fields: %{
+    value: 442_221_834_240
+  },
+  tags: %{
+    hostname: "server01"
+  },
+  timestamp: 1_435_362_189_575_692_182
+}
 
-  @point_escaping %{
-    measurement: ~S|"measurement with quotes"|,
-    tags: %{
-      "tag key with spaces" => ~S|tag,value,with"commas"|
-    },
-    fields: %{
-      ~S|field_key\\\\| => ~S|string field value, only " need be quoted|
-    },
-    timestamp: nil
-  }
+point_escaping = %{
+  measurement: ~S|"measurement with quotes"|,
+  tags: %{
+    "tag key with spaces" => ~S|tag,value,with"commas"|
+  },
+  fields: %{
+    ~S|field_key\\\\| => ~S|string field value, only " need be quoted|
+  },
+  timestamp: nil
+}
 
-  @point_simple %{
-    measurement: "disk_free",
-    fields: %{
-      value: 442_221_834_240
-    },
-    timestamp: nil
-  }
+point_simple = %{
+  measurement: "disk_free",
+  fields: %{
+    value: 442_221_834_240
+  },
+  timestamp: nil
+}
 
-  def run do
-    Benchee.run(
-      %{
-        "Encoding" => &Line.encode/1
-      },
-      inputs: %{
-        "complete" => [@point_complete],
-        "escaping" => [@point_escaping],
-        "multiple" => List.duplicate(@point_simple, 50),
-        "simple" => [@point_simple]
-      },
-      formatters: [{Benchee.Formatters.Console, comparison: false}],
-      warmup: 2,
-      time: 10
-    )
-  end
-end
-
-Instream.Benchmark.LineEncoder.run()
+Benchee.run(
+  %{
+    "Encoding" => &Line.encode/1
+  },
+  inputs: %{
+    "complete" => [point_complete],
+    "escaping" => [point_escaping],
+    "multiple" => List.duplicate(point_simple, 50),
+    "simple" => [point_simple]
+  },
+  formatters: [{Benchee.Formatters.Console, comparison: false}],
+  warmup: 2,
+  time: 10
+)
