a66baff5ac3e72cda4f0e839167b1776c0ac404f
Marc Neudert <marc.neudert@gmail.com>
2020-12-10 18:14:39 +0100

    Clean up JSON decoder/encoder MFArgs resolution test

--- test/instream/connection/json_test.exs
+++ test/instream/connection/json_test.exs
@@ -1,15 +1,15 @@
 defmodule Instream.Connection.JSONTest do
   use ExUnit.Case, async: true
 
+  alias Instream.Connection.JSON
+
   defmodule JSONConnectionModule do
     alias Instream.Connection.JSONTest.JSONLibrary
 
     use Instream.Connection,
-      otp_app: :instream,
       config: [
         json_decoder: JSONLibrary,
-        json_encoder: JSONLibrary,
-        loggers: []
+        json_encoder: JSONLibrary
       ]
   end
 
@@ -17,11 +17,9 @@ defmodule Instream.Connection.JSONTest do
     alias Instream.Connection.JSONTest.JSONLibrary
 
     use Instream.Connection,
-      otp_app: :instream,
       config: [
         json_decoder: {JSONLibrary, :decode_partial},
-        json_encoder: {JSONLibrary, :encode_partial},
-        loggers: []
+        json_encoder: {JSONLibrary, :encode_partial}
       ]
   end
 
@@ -29,74 +27,29 @@ defmodule Instream.Connection.JSONTest do
     alias Instream.Connection.JSONTest.JSONLibrary
 
     use Instream.Connection,
-      otp_app: :instream,
       config: [
         json_decoder: {JSONLibrary, :decode_full, [[keys: :atoms]]},
-        json_encoder: {JSONLibrary, :encode_full, [[foo: :bar]]},
-        loggers: []
+        json_encoder: {JSONLibrary, :encode_full, [[foo: :bar]]}
       ]
   end
 
   defmodule JSONLibrary do
-    alias Instream.Connection.JSONTest.JSONLogger
+    def decode!(_), do: %{decode: :decode!}
+    def decode_partial(_), do: %{decode: :decode_partial}
+    def decode_full(_, keys: :atoms), do: %{decode: :decode_full}
 
-    def decode!(data) do
-      JSONLogger.log(:decode_module)
-      Jason.decode!(data, keys: :atoms)
-    end
-
-    def decode_partial(data) do
-      JSONLogger.log(:decode_partial)
-      Jason.decode!(data, keys: :atoms)
-    end
-
-    def decode_full(data, keys: :atoms) do
-      JSONLogger.log(:decode_full)
-      Jason.decode!(data, keys: :atoms)
-    end
-
-    def encode!(data) do
-      JSONLogger.log(:encode_module)
-      Jason.encode!(data)
-    end
-
-    def encode_partial(data) do
-      JSONLogger.log(:encode_partial)
-      Jason.encode!(data)
-    end
-
-    def encode_full(data, foo: :bar) do
-      JSONLogger.log(:encode_full)
-      Jason.encode!(data)
-    end
-  end
-
-  defmodule JSONLogger do
-    use Agent
-
-    def start_link(_), do: Agent.start_link(fn -> [] end, name: __MODULE__)
-
-    def log(action), do: Agent.update(__MODULE__, fn actions -> [action | actions] end)
-    def flush, do: Agent.get_and_update(__MODULE__, &{&1, []})
+    def encode!(_), do: "encode!"
+    def encode_partial(_), do: "encode_partial"
+    def encode_full(_, foo: :bar), do: "encode_full"
   end
 
   test "json runtime configuration" do
-    {:ok, _} = start_supervised(JSONLogger)
-    {:ok, _} = start_supervised(JSONConnectionFull)
-    {:ok, _} = start_supervised(JSONConnectionModule)
-    {:ok, _} = start_supervised(JSONConnectionPartial)
+    assert %{decode: :decode!} = JSON.decode("test", JSONConnectionModule)
+    assert %{decode: :decode_partial} = JSON.decode("test", JSONConnectionPartial)
+    assert %{decode: :decode_full} = JSON.decode("test", JSONConnectionFull)
 
-    _ = JSONConnectionModule.query("", params: %{})
-    _ = JSONConnectionPartial.query("", params: %{})
-    _ = JSONConnectionFull.query("", params: %{})
-
-    assert [
-             :decode_full,
-             :encode_full,
-             :decode_partial,
-             :encode_partial,
-             :decode_module,
-             :encode_module
-           ] = JSONLogger.flush()
+    assert "encode!" = JSON.encode(%{foo: :bar}, JSONConnectionModule)
+    assert "encode_partial" = JSON.encode(%{foo: :bar}, JSONConnectionPartial)
+    assert "encode_full" = JSON.encode(%{foo: :bar}, JSONConnectionFull)
   end
 end
