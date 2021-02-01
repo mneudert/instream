6df599c98931b0a78082d148265fc6f778f4453a
Marc Neudert <marc.neudert@gmail.com>
2021-02-01 19:15:34 +0100

    Separate basic query/write tests for InfluxDB v1/v2

--- test/influxdb_v1/connection_test.exs
+++ test/influxdb_v1/connection_test.exs
@@ -5,6 +5,21 @@ defmodule Instream.InfluxDBv1.ConnectionTest do
 
   alias Instream.TestHelpers.Connections.DefaultConnection
 
+  defmodule TestSeries do
+    use Instream.Series
+
+    series do
+      measurement "data_write_struct"
+
+      tag :bar
+      tag :foo
+
+      field :value
+    end
+  end
+
+  @tags %{foo: "foo", bar: "bar"}
+
   test "ping connection" do
     assert :pong = DefaultConnection.ping()
   end
@@ -15,5 +30,88 @@ defmodule Instream.InfluxDBv1.ConnectionTest do
 
   test "version connection" do
     assert is_binary(DefaultConnection.version())
+  end
+
+  test "read using params" do
+    test_field = ~S|string field value, only " need be quoted|
+    test_tag = ~S|tag,value,with"commas"|
+
+    :ok =
+      DefaultConnection.write([
+        %{
+          measurement: "params",
+          tags: %{foo: test_tag},
+          fields: %{value: test_field}
+        }
+      ])
+
+    query = "SELECT value FROM params WHERE foo = $foo_val"
+    params = %{foo_val: test_tag}
+
+    assert %{results: [%{series: [%{name: "params", values: [[_, ^test_field]]}]}]} =
+             DefaultConnection.query(query, params: params)
+  end
+
+  @tag :"influxdb_include_1.8"
+  test "read using flux query" do
+    :ok =
+      DefaultConnection.write([
+        %{
+          measurement: "flux",
+          tags: %{foo: "bar"},
+          fields: %{value: 1}
+        }
+      ])
+
+    query = ~S[
+      from(bucket:"test_database/autogen")
+      |> range(start: -1h)
+      |> filter(fn: (r) => r._measurement == "flux")
+    ]
+
+    result = DefaultConnection.query(query, query_language: :flux)
+
+    assert "#datatype," <> _ = result
+
+    assert String.contains?(result, "flux,bar")
+    assert String.contains?(result, "_measurement,foo")
+  end
+
+  test "write data" do
+    measurement = "write_data"
+
+    :ok =
+      DefaultConnection.write([
+        %{
+          measurement: measurement,
+          tags: @tags,
+          fields: %{value: 0.66}
+        }
+      ])
+
+    assert %{results: [%{series: [%{tags: values_tags, values: value_rows}]}]} =
+             DefaultConnection.query("SELECT * FROM #{measurement} GROUP BY *")
+
+    assert @tags == values_tags
+    assert 0 < length(value_rows)
+  end
+
+  test "writing series struct" do
+    :ok =
+      %{
+        bar: "bar",
+        foo: "foo",
+        value: 17
+      }
+      |> TestSeries.from_map()
+      |> DefaultConnection.write()
+
+    assert %{results: [%{series: [%{tags: values_tags, values: value_rows}]}]} =
+             DefaultConnection.query(
+               "SELECT * FROM #{TestSeries.__meta__(:measurement)} GROUP BY *"
+             )
+
+    assert @tags == values_tags
+    assert 0 < length(value_rows)
   end
 end
--- test/influxdb_v2/connection_test.exs
+++ test/influxdb_v2/connection_test.exs
@@ -5,9 +5,78 @@ defmodule Instream.InfluxDBv2.ConnectionTest do
 
   alias Instream.TestHelpers.Connections.DefaultConnection
 
+  defmodule TestSeries do
+    use Instream.Series
+
+    series do
+      measurement "data_write_struct"
+
+      tag :bar
+      tag :foo
+
+      field :value
+    end
+  end
+
+  @tags %{foo: "foo", bar: "bar"}
+
   test "mismatched InfluxDB version" do
     assert {:error, :version_mismatch} = DefaultConnection.ping()
     assert {:error, :version_mismatch} = DefaultConnection.status()
     assert {:error, :version_mismatch} = DefaultConnection.version()
+  end
+
+  test "write data" do
+    measurement = "write_data"
+
+    :ok =
+      DefaultConnection.write([
+        %{
+          measurement: measurement,
+          tags: @tags,
+          fields: %{value: 0.66}
+        }
+      ])
+
+    result =
+      DefaultConnection.query(
+        """
+          from(bucket: "#{DefaultConnection.config(:bucket)}")
+          |> range(start: -5m)
+          |> filter(fn: (r) =>
+            r._measurement == "#{measurement}"
+          )
+        """,
+        query_language: :flux
+      )
+
+    assert String.contains?(result, "_value,_field,_measurement,bar,foo")
+    assert String.contains?(result, "0.66,value,#{measurement},bar,foo")
+  end
+
+  test "writing series struct" do
+    :ok =
+      %{
+        bar: "bar",
+        foo: "foo",
+        value: 17
+      }
+      |> TestSeries.from_map()
+      |> DefaultConnection.write()
+
+    result =
+      DefaultConnection.query(
+        """
+          from(bucket: "#{DefaultConnection.config(:bucket)}")
+          |> range(start: -5m)
+          |> filter(fn: (r) =>
+            r._measurement == "#{TestSeries.__meta__(:measurement)}"
+          )
+        """,
+        query_language: :flux
+      )
+
+    assert String.contains?(result, "_value,_field,_measurement,bar,foo")
+    assert String.contains?(result, "17,value,#{TestSeries.__meta__(:measurement)},bar,foo")
   end
 end
