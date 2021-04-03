428764cb61fd60e983723ab0c27e61cba863c112
Marc Neudert <marc.neudert@gmail.com>
2021-04-03 10:50:44 +0200

    Stub csv-to-map conversion

---
+++ lib/instream/decoder/csv.ex
@@ -0,0 +1,19 @@
+defmodule Instream.Decoder.CSV do
+  @moduledoc false
+
+  NimbleCSV.define(__MODULE__.Parser, separator: ",", escape: "\"")
+
+  @doc """
+  Converts a full CSV response into a list of maps.
+  """
+  @spec parse(binary) :: [map]
+  def parse(response) do
+    case __MODULE__.Parser.parse_string(response, skip_headers: false) do
+      [[_ | _] = headers | [_ | _] = rows] ->
+        Enum.map(rows, fn row -> headers |> Enum.zip(row) |> Map.new() end)
+
+      _ ->
+        []
+    end
+  end
+end
---
+++ test/instream/decoder/csv_test.exs
@@ -0,0 +1,80 @@
+defmodule Instream.Decoder.LineTest do
+  use ExUnit.Case
+
+  alias Instream.Decoder.CSV
+
+  test "single schema decoding" do
+    response = """
+    result,table,_start,_stop,_time,region,host,_value
+    my-result,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:00Z,east,A,15.43
+    my-result,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:20Z,east,B,59.25
+    my-result,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:40Z,east,C,52.62
+    my-result,1,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:00Z,west,A,62.73
+    my-result,1,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:20Z,west,B,12.83
+    my-result,1,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:40Z,west,C,51.62
+    """
+
+    assert [
+             %{
+               "result" => "my-result",
+               "table" => "0",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:00Z",
+               "region" => "east",
+               "host" => "A",
+               "_value" => "15.43"
+             },
+             %{
+               "result" => "my-result",
+               "table" => "0",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:20Z",
+               "region" => "east",
+               "host" => "B",
+               "_value" => "59.25"
+             },
+             %{
+               "result" => "my-result",
+               "table" => "0",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:40Z",
+               "region" => "east",
+               "host" => "C",
+               "_value" => "52.62"
+             },
+             %{
+               "result" => "my-result",
+               "table" => "1",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:00Z",
+               "region" => "west",
+               "host" => "A",
+               "_value" => "62.73"
+             },
+             %{
+               "result" => "my-result",
+               "table" => "1",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:20Z",
+               "region" => "west",
+               "host" => "B",
+               "_value" => "12.83"
+             },
+             %{
+               "result" => "my-result",
+               "table" => "1",
+               "_start" => "2018-05-08T20:50:00Z",
+               "_stop" => "2018-05-08T20:51:00Z",
+               "_time" => "2018-05-08T20:50:40Z",
+               "region" => "west",
+               "host" => "C",
+               "_value" => "51.62"
+             }
+           ] = CSV.parse(response)
+  end
+end
