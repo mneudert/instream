37171f5cb7a119ad013e4cfd2815f9c690d8ee1a
Marc Neudert <marc.neudert@gmail.com>
2021-04-19 19:34:05 +0200

    Use complete alias for test config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 if Mix.env() == :test do
-  alias Instream.TestHelpers
+  alias Instream.TestHelpers.Connections.DefaultConnection
 
   config :logger, :console,
     format: "\n$time $metadata[$level] $levelpad$message\n",
@@ -9,13 +9,13 @@ if Mix.env() == :test do
 
   case System.get_env("INFLUXDB_TOKEN") do
     nil ->
-      config :instream, TestHelpers.Connections.DefaultConnection,
+      config :instream, DefaultConnection,
         auth: [username: "instream_test", password: "instream_test"],
         database: "test_database",
         loggers: []
 
     token ->
-      config :instream, TestHelpers.Connections.DefaultConnection,
+      config :instream, DefaultConnection,
         auth: [method: :token, token: token],
         bucket: "test_database",
         org: "instream_test",
