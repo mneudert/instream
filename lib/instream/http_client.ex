fe77496adc22088ea5cf5c6cc8707211680a7375
Marc Neudert <marc.neudert@gmail.com>
2021-09-14 19:07:51 +0200

    Explicitly typespec values for "method" in HTTPClient.request/5

--- lib/instream/http_client.ex
+++ lib/instream/http_client.ex
@@ -3,11 +3,18 @@ defmodule Instream.HTTPClient do
   Behaviour definition for pluggable HTTP clients.
   """
 
+  @type method :: :get | :head | :post
   @type response ::
           {:ok, status :: pos_integer, headers :: list}
           | {:ok, status :: pos_integer, headers :: list, body :: binary}
           | {:error, term}
 
-  @callback request(method :: atom, url :: binary, headers :: list, body :: binary, opts :: list) ::
+  @callback request(
+              method :: method,
+              url :: binary,
+              headers :: list,
+              body :: binary,
+              opts :: list
+            ) ::
               response
 end
