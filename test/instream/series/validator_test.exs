b2df737d505e4f74b66e8a07560782b093289051
Marc Neudert <marc.neudert@gmail.com>
2021-09-19 13:09:18 +0200

    Restructure series validation tests

--- test/instream/series/validator_test.exs
+++ test/instream/series/validator_test.exs
@@ -16,120 +16,124 @@ defmodule Instream.Series.ValidatorTest do
     end
   end
 
-  test "forbidden field :_field raises" do
-    assert_raise ArgumentError, ~r/forbidden field.+_field/, fn ->
-      defmodule ForbiddenFieldField do
-        use Instream.Series
+  describe "forbiddent values raise" do
+    test "field :_field" do
+      assert_raise ArgumentError, ~r/forbidden field.+_field/, fn ->
+        defmodule ForbiddenFieldField do
+          use Instream.Series
 
-        series do
-          measurement "satisfy_definition_rules"
+          series do
+            measurement "satisfy_definition_rules"
 
-          field :_field
+            field :_field
+          end
+        end
+      end
+    end
+
+    test "field :_measurement" do
+      assert_raise ArgumentError, ~r/forbidden field.+_measurement/, fn ->
+        defmodule ForbiddenMeasurementField do
+          use Instream.Series
+
+          series do
+            measurement "satisfy_definition_rules"
+
+            field :_measurement
+          end
+        end
+      end
+    end
+
+    test "field :time" do
+      assert_raise ArgumentError, ~r/forbidden field.+time/, fn ->
+        defmodule ForbiddenTimeField do
+          use Instream.Series
+
+          series do
+            measurement "satisfy_definition_rules"
+
+            field :time
+          end
+        end
+      end
+    end
+
+    test "tag :_field" do
+      assert_raise ArgumentError, ~r/forbidden tag.+_field/, fn ->
+        defmodule ForbiddenFieldTag do
+          use Instream.Series
+
+          series do
+            measurement "satisfy_definition_rules"
+
+            field :satisfy_definition
+            tag :_field
+          end
+        end
+      end
+    end
+
+    test "tag :_measurement" do
+      assert_raise ArgumentError, ~r/forbidden tag.+_measurement/, fn ->
+        defmodule ForbiddenMeasurementTag do
+          use Instream.Series
+
+          series do
+            measurement "satisfy_definition_rules"
+
+            field :satisfy_definition
+            tag :_measurement
+          end
+        end
+      end
+    end
+
+    test "tag :time" do
+      assert_raise ArgumentError, ~r/forbidden tag.+time/, fn ->
+        defmodule ForbiddenTimeTag do
+          use Instream.Series
+
+          series do
+            measurement "satisfy_definition_rules"
+
+            field :satisfy_definition
+            tag :time
+          end
         end
       end
     end
   end
 
-  test "forbidden field :_measurement raises" do
-    assert_raise ArgumentError, ~r/forbidden field.+_measurement/, fn ->
-      defmodule ForbiddenMeasurementField do
-        use Instream.Series
+  describe "missing values raise" do
+    test "fields" do
+      assert_raise ArgumentError, ~r/no fields/, fn ->
+        defmodule MissingFields do
+          use Instream.Series
 
-        series do
-          measurement "satisfy_definition_rules"
-
-          field :_measurement
+          series do
+            measurement "satisfy_definition_rules"
+          end
         end
       end
     end
-  end
 
-  test "forbidden field :time raises" do
-    assert_raise ArgumentError, ~r/forbidden field.+time/, fn ->
-      defmodule ForbiddenTimeField do
-        use Instream.Series
+    test "measurement" do
+      assert_raise ArgumentError, ~r/missing measurement/, fn ->
+        defmodule MissingMeasurement do
+          use Instream.Series
 
-        series do
-          measurement "satisfy_definition_rules"
-
-          field :time
+          series do
+          end
         end
       end
     end
-  end
 
-  test "forbidden tag :_field raises" do
-    assert_raise ArgumentError, ~r/forbidden tag.+_field/, fn ->
-      defmodule ForbiddenFieldTag do
-        use Instream.Series
-
-        series do
-          measurement "satisfy_definition_rules"
-
-          field :satisfy_definition
-          tag :_field
+    test "series" do
+      assert_raise ArgumentError, ~r/missing series/, fn ->
+        defmodule MissingSeries do
+          use Instream.Series
         end
-      end
-    end
-  end
-
-  test "forbidden tag :_measurement raises" do
-    assert_raise ArgumentError, ~r/forbidden tag.+_measurement/, fn ->
-      defmodule ForbiddenMeasurementTag do
-        use Instream.Series
-
-        series do
-          measurement "satisfy_definition_rules"
-
-          field :satisfy_definition
-          tag :_measurement
-        end
-      end
-    end
-  end
-
-  test "forbidden tag :time raises" do
-    assert_raise ArgumentError, ~r/forbidden tag.+time/, fn ->
-      defmodule ForbiddenTimeTag do
-        use Instream.Series
-
-        series do
-          measurement "satisfy_definition_rules"
-
-          field :satisfy_definition
-          tag :time
-        end
-      end
-    end
-  end
-
-  test "missing fields raises" do
-    assert_raise ArgumentError, ~r/no fields/, fn ->
-      defmodule MissingFields do
-        use Instream.Series
-
-        series do
-          measurement "satisfy_definition_rules"
-        end
-      end
-    end
-  end
-
-  test "missing measurement raises" do
-    assert_raise ArgumentError, ~r/missing measurement/, fn ->
-      defmodule MissingMeasurement do
-        use Instream.Series
-
-        series do
-        end
-      end
-    end
-  end
-
-  test "missing series raises" do
-    assert_raise ArgumentError, ~r/missing series/, fn ->
-      defmodule MissingSeries do
-        use Instream.Series
       end
     end
   end
