32b44e37b9bb1755fe001b82d33733cee3eb0f13
Marc Neudert <marc.neudert@gmail.com>
2021-11-02 18:00:33 +0100

    Switch back to Mix.env/0 for Elixir < 1.11

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-if config_env() == :test do
+if Mix.env() == :test do
   config :logger, :console,
     format: "\n$time $metadata[$level] $levelpad$message\n",
     metadata: [:query_time, :response_status]
