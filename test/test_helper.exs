79f6f41d9cee70101505629867894049a1f5ca69
Marc Neudert <marc.neudert@gmail.com>
2022-08-19 13:51:07 +0200

    Prepare testing InfluxDB v2.4.x

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -13,6 +13,7 @@ version_excludes =
     "2.1" -> [:"influxdb_exclude_2.1", :"influxdb_exclude_2.x", :"influxdb_include_1.x"]
     "2.2" -> [:"influxdb_exclude_2.2", :"influxdb_exclude_2.x", :"influxdb_include_1.x"]
     "2.3" -> [:"influxdb_exclude_2.3", :"influxdb_exclude_2.x", :"influxdb_include_1.x"]
+    "2.4" -> [:"influxdb_exclude_2.4", :"influxdb_exclude_2.x", :"influxdb_include_1.x"]
     _ -> raise RuntimeError, "Unknown INFLUXDB_VERSION: #{inspect(version)}"
   end
 
@@ -21,7 +22,7 @@ config = Keyword.put(config, :exclude, version_excludes ++ (config[:exclude] || 
 IO.puts("Running tests for InfluxDB version: #{version}")
 
 # configure InfluxDB connection
-if version in ["2.0", "2.1", "2.2", "2.3"] do
+if version in ["2.0", "2.1", "2.2", "2.3", "2.4"] do
   Application.put_env(
     :instream,
     TestConnection,
