507c5860d28cd11fd4ae0454729162b7bd884b80
Marc Neudert <marc.neudert@gmail.com>
2023-04-22 12:09:04 +0200

    Bump version to v2.2.1-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Instream.MixProject do
 
   @url_changelog "https://hexdocs.pm/instream/changelog.html"
   @url_github "https://github.com/mneudert/instream"
-  @version "2.2.0"
+  @version "2.2.1-dev"
 
   def project do
     [
