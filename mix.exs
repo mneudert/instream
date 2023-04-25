f32127c7ebffac790317be1f56380f8a9d803b67
Marc Neudert <marc.neudert@gmail.com>
2023-04-25 18:19:12 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -43,10 +43,10 @@ defmodule Instream.MixProject do
   defp deps do
     [
       {:benchee, "~> 1.1", only: :bench, runtime: false},
-      {:credo, "~> 1.6", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.2", only: :test, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.3", only: :test, runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.15.0", only: :test, runtime: false},
+      {:excoveralls, "~> 0.16.0", only: :test, runtime: false},
       {:hackney, "~> 1.1"},
       {:influxql, "~> 0.2.0"},
       {:jason, "~> 1.0"},
